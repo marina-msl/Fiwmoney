08a207c4f9b5223d95a4ce1e08197b913c894527
Marina Leide <marina.msleide@gmail.com>
2025-05-12 22:32:42 -0300

    Implementing method to check the stock's current price every one hour

--- backend/src/main/java/com/mmsl/fiwmoney/service/StockService.java
+++ backend/src/main/java/com/mmsl/fiwmoney/service/StockService.java
@@ -2,6 +2,7 @@ package com.mmsl.fiwmoney.service;
 
 import java.util.List;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -24,6 +25,23 @@ public class StockService {
 
     public List<Stock> findAll() {
         return repository.findAll();
+    }
+
+    @Scheduled(fixedRate=3600000)
+    public void updateStockPrices() {
+        List<Stock> stocks = repository.findAll();
+
+        for (Stock stock : stocks) {
+            double currentPrice = fetchCurrentPrice(stock.getCode());
+
+            if (currentPrice != -1) {
+                stock.setCurrentPrice(currentPrice);
+                repository.save(stock);
+                System.out.println("Update price for: " + stock.getCode());
+            } else {
+                System.out.println("Stock not found: " + stock.getCode());
+            }
+        }
     }
 
     public double fetchCurrentPrice(String stockCode) {
