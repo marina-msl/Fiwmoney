84f81781a0a43db49656c0eeec4d9cddb4b19c39
Marina Leide <marina.msleide@gmail.com>
2025-06-03 22:18:11 -0300

    Correcting CORS problems

--- backend/src/main/java/com/mmsl/fiwmoney/controller/StockController.java
+++ backend/src/main/java/com/mmsl/fiwmoney/controller/StockController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,7 +23,7 @@ public class StockController {
     @Autowired
     private StockService service;
 
-    // @CrossOrigin(origins = "http://localhost:9000")
+    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping(value = "/stock")
     public ResponseEntity<StockResult> getStock(@RequestBody StockRequest stock) {
         if (stock.getCode() == null) {
