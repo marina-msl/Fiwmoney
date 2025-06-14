c125933755283c8ece9beedd05ac33cca1736333
Marina Leide <marina.msleide@gmail.com>
2025-06-13 23:46:11 -0300

    Implementing queryto update notify value on db

--- backend/src/main/java/com/mmsl/fiwmoney/repository/StockRepository.java
+++ backend/src/main/java/com/mmsl/fiwmoney/repository/StockRepository.java
@@ -1,6 +1,9 @@
 package com.mmsl.fiwmoney.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.mmsl.fiwmoney.model.Stock;
@@ -8,11 +11,8 @@ import com.mmsl.fiwmoney.model.Stock;
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
-   // List<Stock> findByName(String name);
-
-   // Optional<Stock> findById(Long id);
-
-  //  public Stock findByCode(String code);
-
+  @Modifying
+  @Query("UPDATE Stock s SET s.notify = :notify WHERE s.code = :code")
+  void updateNotify(@Param("code") String code, @Param("notify") boolean notify);
 
 }
