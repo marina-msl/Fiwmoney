b5190c108c832b102b63d982b7ae4dbf51a1a0ef
Marina Leide <marina.msleide@gmail.com>
2025-06-13 22:44:42 -0300

    Implementing global cors configuration

--- backend/src/main/java/com/mmsl/fiwmoney/controller/StockController.java
+++ backend/src/main/java/com/mmsl/fiwmoney/controller/StockController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,7 +24,7 @@ public class StockController {
     @Autowired
     private StockService service;
 
-    @CrossOrigin(origins = "http://localhost:5173")
+    // @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping(value = "/stock")
     public ResponseEntity<StockResult> getStock(@RequestBody StockRequest stock) {
         if (stock.getCode() == null) {
---
+++ backend/src/main/java/com/mmsl/fiwmoney/service/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.mmsl.fiwmoney.service;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer  {
+
+    private static final String FRONTEND_URL = "http://localhost:5173";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(FRONTEND_URL)
+                .allowedMethods("GET", "POST")
+                .allowedHeaders("*");
+    }
+    
+}
