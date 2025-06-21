b26939c262b0421cad04d718336031479b8bf85c
Marina Leide <marina.msleide@gmail.com>
2025-06-20 21:08:52 -0300

    Implementing User entity

---
+++ backend/src/main/java/com/mmsl/fiwmoney/model/User.java
@@ -0,0 +1,13 @@
+package com.mmsl.fiwmoney.model;
+
+import java.util.List;
+
+public class User {
+    
+    private long id;
+    private String name;
+    private String email;
+    private String cellphoneNumber;
+    private List<Stock> stocks;
+
+}
