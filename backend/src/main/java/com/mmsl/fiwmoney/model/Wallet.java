37601177c9afd72e47a8c3ab6d305a627843c25e
Marina Leide <marina.msleide@gmail.com>
2025-10-15 16:05:11 -0300

    Encapsulating Stock collection

--- backend/src/main/java/com/mmsl/fiwmoney/model/Wallet.java
+++ backend/src/main/java/com/mmsl/fiwmoney/model/Wallet.java
@@ -1,9 +1,37 @@
 package com.mmsl.fiwmoney.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Wallet {
 
     private Long id;
-    // private <List> StocksHold;
+    private List<Stock> stocks;
+
+    public Wallet() {
+        this.stocks = new ArrayList<>();
+    }
+
+    public Wallet(Long id, List<Stock> stocks) {
+        this.id = id;
+        this.stocks = stocks;
+    }
+
+    public void addStock(Stock stock) {
+        this.stocks.add(stock);
+    }
+
+    public void removeStock(Stock stock) {
+        this.stocks.remove(stock);
+    }
+
+    public void replace(List<Stock> stocks) {
+        this.stocks = stocks;
+    }
+
+    public List <Stock> getStocks() {
+        return new ArrayList<>(this.stocks);
+    }
 
     
 }
