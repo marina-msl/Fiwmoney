cb800a58bfed92740ce31dcded670fb2eea198b3
Marina Leide <marina.msleide@gmail.com>
2025-10-23 15:06:41 -0300

    Commenting in order to execute the app correctly

--- backend/src/main/java/com/mmsl/fiwmoney/model/User.java
+++ backend/src/main/java/com/mmsl/fiwmoney/model/User.java
@@ -5,15 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Entity
-public class User {
+// @Entity
+// public class User {
     
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    private String name;
-    private String email;
-    private String cellphoneNumber;
-    // private Wallet wallet;
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private long id;
+//     private String name;
+//     private String email;
 
 }
