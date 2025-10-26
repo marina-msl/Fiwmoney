04c66955b95327e25f6fb2f6af5c4ba3f91281a5
Marina Leide <marina.msleide@gmail.com>
2025-10-26 19:15:56 -0300

    Implementing DTO as a record

--- backend/src/main/java/com/mmsl/fiwmoney/dto/UserDto.java
+++ backend/src/main/java/com/mmsl/fiwmoney/dto/UserDto.java
@@ -1,14 +1,9 @@
 package com.mmsl.fiwmoney.dto;
 
-import lombok.Data;
-
-@Data
-public class UserDto {
-    
-    private String username;
-    private String password;
-    private String name;
-    private String email;
-    private String tenantId;
-
-}
+public record  UserDto (
+    String username,
+    String password,
+    String name,
+    String email,
+    String tenantId
+) {}
