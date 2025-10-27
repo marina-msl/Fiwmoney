2f8c4d9ec692ac67d1cb44c50ebfb5e5133117cb
Marina Leide <marina.msleide@gmail.com>
2025-10-26 22:47:22 -0300

    Solve exception of the keys's size

--- backend/src/main/java/com/mmsl/fiwmoney/util/JwtUtil.java
+++ backend/src/main/java/com/mmsl/fiwmoney/util/JwtUtil.java
@@ -2,30 +2,31 @@ package com.mmsl.fiwmoney.util;
 
 import java.util.Date;
 
+import javax.crypto.SecretKey;
+
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtUtil {
     
-    private final String SECRET = "mysupersecretkey";
+    private final SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     public String generateToken(String username, String tenantId) {
         return Jwts.builder()
                 .setSubject(username)
                 .claim("tenant", tenantId)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 day
-                .signWith(Keys.hmacShaKeyFor(SECRET.getBytes()))
+                .signWith(key)
                 .compact();
     }
 
     public Claims extractClaims(String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(Keys.hmacShaKeyFor(SECRET.getBytes()))
+                .setSigningKey(key)
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
