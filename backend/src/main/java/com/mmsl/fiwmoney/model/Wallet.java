04fdb701d1501216f7fcf850258e823870cac41f
Marina Leide <marina.msleide@gmail.com>
2025-11-17 22:51:33 -0300

    Correct imports

--- backend/src/main/java/com/mmsl/fiwmoney/model/Wallet.java
+++ backend/src/main/java/com/mmsl/fiwmoney/model/Wallet.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
