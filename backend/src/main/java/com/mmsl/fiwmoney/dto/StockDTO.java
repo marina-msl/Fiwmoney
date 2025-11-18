53c6aa2711bb0fec0e33cbb6eee55329cdf341c1
Marina Leide <marina.msleide@gmail.com>
2025-11-17 22:50:04 -0300

    Remove commented code

--- backend/src/main/java/com/mmsl/fiwmoney/dto/StockDTO.java
+++ backend/src/main/java/com/mmsl/fiwmoney/dto/StockDTO.java
@@ -12,26 +12,4 @@ public class StockDTO {
     private double averagePrice;
     private boolean notify;
 
-    // public StockDTO(String code, double currentPrice, double averagePrice, boolean notify) {
-    //     this.code = code;
-    //     this.currentPrice = currentPrice;
-    //     this.averagePrice = averagePrice;
-    //     this.notify = notify;
-    // }
-
-    // public String getCode() {
-    //     return code;
-    // }
-
-    // public double getCurrentPrice() {
-    //     return currentPrice;
-    // }
-
-    // public double getAveragePrice() {
-    //     return averagePrice;
-    // }
-
-    // public boolean getNotify() {
-    //     return notify;
-    // }
 }
