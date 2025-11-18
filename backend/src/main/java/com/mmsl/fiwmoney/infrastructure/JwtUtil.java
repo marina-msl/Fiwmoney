702decdd9ab3e67a431027771afdc4ab7374f0a4
Marina Leide <marina.msleide@gmail.com>
2025-11-17 22:38:10 -0300

    A better name for the Utils, trying to follow hexagonal architecture

---
+++ backend/src/main/java/com/mmsl/fiwmoney/infrastructure/JwtUtil.java
@@ -0,0 +1,31 @@
+package com.mmsl.fiwmoney.infrastructure;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+    
+    private final SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .signWith(key)
+                .compact();
+    }
+
+    public Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
