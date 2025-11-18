67af97ee0395495af07cc98e58fe1fb366fd38d8
Marina Leide <marina.msleide@gmail.com>
2025-11-17 22:44:36 -0300

    Refactor the controller for new architecture

--- backend/src/main/java/com/mmsl/fiwmoney/controller/WalletController.java
+++ backend/src/main/java/com/mmsl/fiwmoney/controller/WalletController.java
@@ -4,29 +4,36 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mmsl.fiwmoney.dto.StockDTO;
 import com.mmsl.fiwmoney.dto.StockRequest;
 import com.mmsl.fiwmoney.dto.StockResult;
+import com.mmsl.fiwmoney.dto.WalletDto;
 import com.mmsl.fiwmoney.model.Stock;
 import com.mmsl.fiwmoney.service.StockService;
+import com.mmsl.fiwmoney.service.WalletService;
 
 @RestController
 @RequestMapping(value = "/")
-public class StockController {
+public class WalletController {
     
     private static final int NOT_FOUND = -1;
     
     @Autowired
     private StockService service;
 
+    @Autowired
+    private WalletService walletService;
+
+
     @PostMapping(value = "/stocks")
     public ResponseEntity<StockResult> getStock(@RequestBody StockRequest stock) {
+
         if (stock.getCode() == null) {
             return ResponseEntity.badRequest().build();
         }
@@ -40,7 +47,7 @@ public class StockController {
             return ResponseEntity.notFound().build();
         }
 
-        StockResult stockResult = new StockResult(code, currentPrice, averagePrice, notify);
+        StockDTO stockResult = new StockDTO(code, currentPrice, averagePrice, notify);
         service.save(stockResult);
 
        return ResponseEntity.ok()
@@ -48,8 +55,10 @@ public class StockController {
        .body(stockResult);
     }
 
-    @GetMapping(value = "/stocks")
-    public ResponseEntity<List<Stock>> getAll() {
+    // @GetMapping(value = "/stocks")
+    // public ResponseEntity<List<Stock>> getAll() {
+    @GetMapping(value = "/wallet/{id}/stocks") {
+    public ResponseEntity<WalletDto> getStock(@PathVariable ("id") id) {
         List<Stock> stocks = service.findAll();
 
         return stocks.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(stocks);
