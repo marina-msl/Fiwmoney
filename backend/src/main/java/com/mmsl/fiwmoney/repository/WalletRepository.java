4a983a3353d249b72cf41766e581bfbdd43030ea
Your Name <your.email@example.com>
2026-02-21 21:07:44 -0300

    Remove unecessary query

--- backend/src/main/java/com/mmsl/fiwmoney/repository/WalletRepository.java
+++ backend/src/main/java/com/mmsl/fiwmoney/repository/WalletRepository.java
@@ -6,13 +6,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.mmsl.fiwmoney.model.Wallet;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
     
-  @Modifying
-  @Query("UPDATE Stock s SET s.notify = :notify WHERE s.walletId = :walletId  s.code = :code")
-  void updateNotify(@Param("walletId") Long id, @Param("code") String code,
-                    @Param("notify") boolean notify);
+//  @Modifying
+//  @Query("UPDATE Stock s SET s.notify = :notify WHERE s.walletId = :walletId  s.code = :code")
+//  void updateNotify(@Param("walletId") Long id, @Param("code") String code,
+//                    @Param("notify") boolean notify);
 
 
 }
