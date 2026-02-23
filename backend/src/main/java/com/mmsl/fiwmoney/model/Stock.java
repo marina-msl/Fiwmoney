50808974f6b0d677ad37cf4c0d724d056624a730
Marina Leide <marina.msleide@gmail.com>
2026-02-23 16:37:15 -0300

    Remove walletID from stock because was creating duplicate

--- backend/src/main/java/com/mmsl/fiwmoney/model/Stock.java
+++ backend/src/main/java/com/mmsl/fiwmoney/model/Stock.java
@@ -20,7 +20,7 @@ public class Stock {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long walletId;
+//    private Long walletId;
     private String code;
     private BigDecimal currentPrice;
     private BigDecimal averagePrice;
